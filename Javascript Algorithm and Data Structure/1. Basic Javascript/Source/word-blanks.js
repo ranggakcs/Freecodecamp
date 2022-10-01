375b45f6588be87520f73690000de9b66df2feeb
Rangga Kusumah <ranggakusumah94@gmail.com>
2022-10-01 20:37:17 +0700

    source file update

---
+++ Javascript Algorithm and Data Structure/1. Basic Javascript/Source/word-blanks.js
@@ -0,0 +1,10 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " in the park " + myVerb + " " +myAdverb + "."; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks);
